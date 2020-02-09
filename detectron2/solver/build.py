807b12bb3a72b3ecc65a6261e67836cfe78f0b97
Лесцов Борис <b.lestsov@corp.mail.ru>
2020-02-09 04:50:09 +0300

    before cons tests

--- detectron2/solver/build.py
+++ detectron2/solver/build.py
@@ -6,6 +6,23 @@ from detectron2.config import CfgNode
 
 from .lr_scheduler import WarmupCosineLR, WarmupMultiStepLR
 
+from detectron2.utils import comm
+class AccSGD(torch.optim.SGD):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.acc_period = 2
+        self.acc_cnt = 0
+
+    def zero_grad(self):
+        if (self.acc_cnt % self.acc_period) == 0:
+            if comm.is_main_process():
+                print("ZG")
+            super().zero_grad()
+
+    def step(self):
+        if ((self.acc_cnt + 1) % self.acc_period) == 0:
+            super().step()
+        self.acc_cnt += 1
 
 def build_optimizer(cfg: CfgNode, model: torch.nn.Module) -> torch.optim.Optimizer:
     """
@@ -28,7 +45,10 @@ def build_optimizer(cfg: CfgNode, model: torch.nn.Module) -> torch.optim.Optimiz
             weight_decay = cfg.SOLVER.WEIGHT_DECAY_BIAS
         params += [{"params": [value], "lr": lr, "weight_decay": weight_decay}]
 
-    optimizer = torch.optim.SGD(params, lr, momentum=cfg.SOLVER.MOMENTUM)
+    if cfg.SOLVER.ACC:
+        optimizer = AccSGD(params, lr, momentum=cfg.SOLVER.MOMENTUM)
+    else:
+        optimizer = torch.optim.SGD(params, lr, momentum=cfg.SOLVER.MOMENTUM)
     return optimizer
 
 
